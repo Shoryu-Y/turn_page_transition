1842457b693a9d8f1755ce4c02fee07d2894c56b
Shoryu Yamamoto <shoryu13@gmail.com>
2022-06-20 09:15:03 +0900

    Update turn_page_transition.dart

--- lib/turn_page_transition.dart
+++ lib/turn_page_transition.dart
@@ -1,3 +1,6 @@
-export 'package:turn_page_transition/src/turn_page_transitions_theme.dart' show TurnPageTransitionsTheme;
-export 'package:turn_page_transition/src/turn_page_route.dart' show TurnPageRoute;
-export 'package:turn_page_transition/src/turn_page_transition.dart' show TurnPageTransition;
+export 'package:turn_page_transition/src/turn_page_transitions_theme.dart'
+    show TurnPageTransitionsTheme;
+export 'package:turn_page_transition/src/turn_page_route.dart'
+    show TurnPageRoute;
+export 'package:turn_page_transition/src/turn_page_transition.dart'
+    show TurnPageTransition;
