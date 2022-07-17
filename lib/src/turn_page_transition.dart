55c0fdcf1d8f19fc8cddcb46a6bd1fcf564a4f8f
Shoryu Yamamoto <shoryu13@gmail.com>
2022-07-17 14:11:26 +0900

    Update: apply landscape to clip

--- lib/src/turn_page_transition.dart
+++ lib/src/turn_page_transition.dart
@@ -48,20 +48,27 @@ class _PageTurnClipper extends CustomClipper<Path> {
   Path getClip(Size size) {
     final width = size.width;
     final height = size.height;
-    final value = animation.value;
+    final animationProgress = animation.value;
+
+    final progressTurningPoint = 0.1;
+    final vVelocity = 1 / progressTurningPoint;
 
     final path = Path()
-      ..moveTo(width, 0)
-      ..lineTo(0, 0);
-    if (value <= 0.5) {
-      final heightValue = value * 2;
+      ..moveTo(width, 0) // TopRight
+      ..lineTo(0, 0); // TopLeft
+    if (animationProgress <= progressTurningPoint) {
+      final vDistanceRate = vVelocity * animationProgress;
       path
-        ..lineTo(width, height * heightValue)
+        ..lineTo(width, height * vDistanceRate) // BottomRightSide
         ..close();
     } else {
+      final hCorrected = width / animationProgress;
+      final progressSubtractedInitial = animationProgress - progressTurningPoint;
+      final hVelocity = 1 / (1 - progressTurningPoint);
+      final hTurnedDistance = hCorrected * progressSubtractedInitial * hVelocity;
       path
-        ..lineTo(width - width / value * (value - 0.5) * 2, height)
-        ..lineTo(width, height)
+        ..lineTo(width - hTurnedDistance, height) // BottomLeft
+        ..lineTo(width, height) // BottomRight
         ..close();
     }
 
@@ -87,7 +94,7 @@ class _OverleafPainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     final width = size.width;
     final height = size.height;
-    final value = animation.value;
+    final animationProgress = animation.value;
 
     final fillPaint = Paint()
       ..color = color
@@ -98,11 +105,11 @@ class _OverleafPainter extends CustomPainter {
       ..style = PaintingStyle.stroke
       ..strokeWidth = 2;
 
-    final path = Path()..moveTo(width * (1 - value), 0);
+    final path = Path()..moveTo(width * (1 - animationProgress), 0);
 
-    if (value <= 0.5) {
-      final W = width * value;
-      final H = height * value * 2;
+    if (animationProgress <= 0.5) {
+      final W = width * animationProgress;
+      final H = height * animationProgress * 2;
       final x = width - 2 * (W * H * H) / (W * W + H * H);
       final y = 2 * (W * W * H) / (W * W + H * H);
 
@@ -110,30 +117,30 @@ class _OverleafPainter extends CustomPainter {
         ..lineTo(x, y) // ページの角
         ..lineTo(width, H) // ページの右端
         ..close();
-    } else if (value < 1) {
+    } else if (animationProgress < 1) {
       // alias value to simple letters;
       final w2 = width * width;
       final h2 = height * height;
-      final mv = 1 - value;
+      final mv = 1 - animationProgress;
       final mv2 = mv * mv;
 
-      final x1 = width - 2 * width * h2 * value / (w2 * mv2 + h2);
-      final y1 = 2 * w2 * height * value * mv / (w2 * mv2 + h2);
+      final x1 = width - 2 * width * h2 * animationProgress / (w2 * mv2 + h2);
+      final y1 = 2 * w2 * height * animationProgress * mv / (w2 * mv2 + h2);
 
-      final para2 = (2 * value - 1) / value;
-      final x2 = width - 2 * width * h2 * value / (w2 * mv2 + h2) * para2;
+      final para2 = (2 * animationProgress - 1) / animationProgress;
+      final x2 = width - 2 * width * h2 * animationProgress / (w2 * mv2 + h2) * para2;
       final y2 = y1 * para2 + height;
 
       path
         ..lineTo(x1, y1)
         ..lineTo(x2, y2)
-        ..lineTo(width - width * 2 * (value - 0.5), height)
+        ..lineTo(width - width * 2 * (animationProgress - 0.5), height)
         ..close();
     } else {
       path
         ..lineTo(0, 0)
         ..lineTo(0, height)
-        ..lineTo(width * (1 - value), height)
+        ..lineTo(width * (1 - animationProgress), height)
         ..close();
     }
 
