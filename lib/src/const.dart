7dbae5f452d746e43f53968f7abcf9b3ec7d5c89
Mario Vtor <92695296+mariovtor@users.noreply.github.com>
2025-08-06 11:25:58 -0300

    allow to change overleaf border color (#17)

--- lib/src/const.dart
+++ lib/src/const.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
 const defaultOverleafColor = Colors.grey;
+const defaultOverleafBorderColor = Colors.black;
 const defaultAnimationTransitionPoint = 0.1;
 const defaultTransitionDuration = Duration(milliseconds: 300);
