42beab3de72339a095b67c9669b690d82b2d2677
tatsubee <tatsubee@pixiv.co.jp>
2025-08-06 23:09:51 +0900

    Update turn_page_view.dart

--- lib/src/turn_page_view.dart
+++ lib/src/turn_page_view.dart
@@ -1,5 +1,3 @@
-import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
-
 import 'package:flutter/material.dart';
 import 'package:turn_page_transition/src/const.dart';
 import 'package:turn_page_transition/src/turn_direction.dart';
